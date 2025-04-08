bfacddeebd634541244f0390edd5773c9478a400
gvarma28 <gouravswork@gmail.com>
2025-04-08 20:00:48 +0530

    feat: template

--- main.go
+++ main.go
@@ -1,7 +1,28 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
 
 func main() {
 	fmt.Println("Hello World")
+	scanner := bufio.NewScanner(os.Stdin)
+
+	scanner.Scan()
+	t, _ := strconv.Atoi(scanner.Text())
+
+	for i := 0; i < t; i++ {
+		scanner.Scan()
+		temp := strings.Split(scanner.Text(), " ")
+		n, _ := strconv.Atoi(temp[0])
+		k, _ := strconv.Atoi(temp[1])
+
+		scanner.Scan()
+		s := scanner.Text()
+		fmt.Printf("%v, %v, %v\n", n, k, s)
+	}
 }
