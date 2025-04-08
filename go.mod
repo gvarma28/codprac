21d13b46e3fa4b7836723f91ecb7df0244e3f3db
gvarma28 <gouravswork@gmail.com>
2025-04-08 22:29:04 +0530

    feat: adding mod_pow

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/gvarma28/codprac
+
+go 1.23.4
--- main.go
+++ main.go
@@ -22,7 +22,33 @@ func main() {
 		k, _ := strconv.Atoi(temp[1])
 
 		scanner.Scan()
+		inputArr := strings.Split(scanner.Text(), " ")
+		inputIntArr := []int{}
+		for _, val := range inputArr {
+			newVal, _ := strconv.Atoi(val)
+			inputIntArr = append(inputIntArr, newVal)
+		}
+
+		scanner.Scan()
 		s := scanner.Text()
-		fmt.Printf("%v, %v, %v\n", n, k, s)
+		fmt.Printf("%v, %v, %v, %v\n", n, k, s, inputIntArr)
 	}
 }
+
+// compute power while applying modulo
+// e.g. 2^5 with 998244353 mod would be mod_pow(2,5,998244353)
+// func mod_pow(base int64, exp int, mod int64) int64 {
+// 	var result int64 = 1
+// 	base = base % mod
+// 	for {
+// 		if exp <= 0 {
+// 			break
+// 		}
+// 		if exp%2 == 1 {
+// 			result = (result * base) % mod
+// 		}
+// 		exp = exp >> 1
+// 		base = (base * base) % mod
+// 	}
+// 	return result
+// }
