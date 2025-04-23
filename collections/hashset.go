251ba0c3de51641b2d24e14e955c781b306c2cd1
gvarma28 <gouravswork@gmail.com>
2025-04-23 22:17:16 +0530

    feat: adding collection - hashset

---
+++ collections/hashset.go
@@ -0,0 +1,49 @@
+package collections
+
+type MyHashSet struct {
+	buckets [][]int
+	size    int
+}
+
+func Constructor() MyHashSet {
+	const defaultSize = 10007
+	return MyHashSet{
+		size:    defaultSize,
+		buckets: make([][]int, defaultSize),
+	}
+}
+
+func (set *MyHashSet) getHash(key int) int {
+	return key % set.size
+}
+
+func (set *MyHashSet) Add(key int) {
+	hash := set.getHash(key)
+	for _, val := range set.buckets[hash] {
+		if val == key {
+			return
+		}
+	}
+	set.buckets[hash] = append(set.buckets[hash], key)
+}
+
+func (set *MyHashSet) Remove(key int) {
+	hash := set.getHash(key)
+	temp := set.buckets[hash]
+	for i, val := range set.buckets[hash] {
+		if val == key {
+			set.buckets[hash] = append(temp[:i], temp[i+1:]...)
+			return
+		}
+	}
+}
+
+func (set *MyHashSet) Contains(key int) bool {
+	hash := set.getHash(key)
+	for _, val := range set.buckets[hash] {
+		if val == key {
+			return true
+		}
+	}
+	return false
+}
